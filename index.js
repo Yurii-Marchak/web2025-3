609cdb21cb3f91cfd6ca70242a7ca6a9a73fcbd0
Юрій web2025-3 <yuramarchak1234@gmail.com>
2025-04-02 16:44:24 +0300

    Додано головний файл з Commander.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Hello, web2025-3!");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Програма для читання аргументів')
+  .option('-n, --name <type>', 'Ваше ім\'я');
+
+program.parse(process.argv);
+
+if (program.name) {
+  console.log(`Привіт, ${program.name}!`);
+} else {
+  console.log("Привіт, web2025-3!");
+}
